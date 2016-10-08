6c5f07c75f92d46dfdf825ebe8d96cb183606da8
treefroog <crs2017@gmail.com>
2016-10-07 23:13:15 -0400

    Added a __init__ file for importing
    Makes testing possible. Currently Doesn't work because of a Python 3 bug

---
+++ groupme/__init__.py
@@ -0,0 +1,19 @@
+"""
+Group Me API Wrapper
+~~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Group Me API.
+
+:copyright: (c) 2016 treefroog
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'groupme'
+__author__ = 'treefroog'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2016 treefroog'
+__version__ = '0.0.1'
+
+from .group import Group
+from .member import Member
+from .object import Object
