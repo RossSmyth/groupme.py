ae4e3522be674d03ce2e1fc85bd1bd2581d5cf5b
treefroog <crs2017@gmail.com>
2016-10-09 15:51:26 -0400

    Group object is more done

--- groupme/group.py
+++ groupme/group.py
@@ -1,4 +1,6 @@
 from .object import Object
+from .member import Member
+from .message import Preview
 
 class Group(Object):
 	"""Represents a Group Me group.
@@ -19,6 +21,8 @@ class Group(Object):
 	
 	Attributes
 	----------
+	count : int
+		Number of messages sent in group
 	created_at : int
 		The unix time-stamp when the group was made
 	description : str
@@ -29,8 +33,8 @@ class Group(Object):
 		The ID of the group
 	max_members : int
 		The maximum members allowed in the group
-	members : ???
-		List of members (TO BE FINAL LATER)
+	members : list
+		List of :class:`Member`
 	messages : ???
 		??? (TO BE FINAL LATER)
 	name : str
@@ -39,41 +43,49 @@ class Group(Object):
 		When the group is in office mode or not
 	phone_number : str
 		The phone number of the group
+	preview : :class:`Preview`
+		The last message sent in the group. The preview doesn't have as much
+		information as a full :class:`Message` but is similar.
 	share_url : str
 		URL to join the group, can be none if private
 	type : str
 		Whether the group is public or private
 	updated_at : int
-		unix time-stamp of when the group was updated last	
+		Unix time-stamp of when the group was updated last	
 	"""
 	__slots__ = [
-		'created_at', 'description', 'image_url', 'id', 'max_members',
-		'members', 'messages', 'name', 'office_mode', 'phone_number',
-		'share_url', 'type', 'updated_at'
+		'count', 'created_at', 'description', 'image_url', 'id', 'max_members',
+		'members', 'messages', 'name', 'office_mode',
+		'phone_number', 'preview' 'share_url', 'type', 'updated_at'
 		]
 	
 	def __init__(self, **kwargs):
+		
+		super(Group, self).__init__(**kwargs)
+		
+		_members = kwargs.pop('members')
+		_messages = kwargs.pop('messages')
 	
+		self.count = _messages.pop('count')
 		self.created_at = kwargs.pop('created_at')
 		self.description = kwargs.pop('description')
 		self.image_url = kwargs.pop('image_url')
 		self.max_members = kwargs.pop('max_members')
+		self.members = self._get_members(_members)
 		self.name = kwargs.pop('name')
 		self.office_mode = kwargs.pop('office_mode')
 		self.phone_number = kwargs.pop('phone_number')
+		self.preview = Preview(**_messages['preview'],
+						id=_messages['last_message_id']),
+						created_at=_messages['last_message_created_at']
 		self.share_url = kwargs.pop('share_url')
 		self.type = kwargs.pop('type')
 		self.updated_at = kwargs.pop('updated_at')
 		
-		self._members = kwargs.pop('members')
-		self._messages = kwargs.pop('messages')
-		
-	@property
-	def members(self):
-		"""ADD WHEN MEMBER CLASS IS MADE"""
-		
-	@property
-	def messages(self):
-		"""ADD WHEN MESSAGE CLASS IS MADE"""
-		
-		
+	def _get_members(self, member_list):
+		"""Makes a list of member objects"""
+		members = []
+		for member in member_list:
+			member = Member(**member)
+			members.append(member)
+		return members
