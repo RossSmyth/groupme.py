efa655983002490b13f75332e6c821dc0c7ef61e
treefroog <crs2017@gmail.com>
2016-10-16 16:56:36 -0400

    A script to pretty print the json packets

---
+++ Unittests/Pretty Printer.py
@@ -0,0 +1,31 @@
+#To pretty print the json packets easily
+
+import json
+from pprint import pprint
+
+files = ['test_attachments.txt', 'test_group.txt', 'test_message.txt']
+
+print('Key : File\n----------')
+
+for file in files:
+    print("{}   : {}".format(files.index(file), file))
+
+choice = int(input('\n\nWhich file do you want Pretty Printed?\n'))
+
+with open(files[choice]) as file:
+    file = json.load(file)
+    pprint(file)
+
+close = False
+while close == False:
+
+    next_thing = input('Press enter to exit, or type a key to Pretty Print smaller sections\n')
+
+    if next_thing == '':
+        close = True
+
+    try:
+        file = file[next_thing]
+        pprint(file)
+    except KeyError:
+        print('invalid key')
