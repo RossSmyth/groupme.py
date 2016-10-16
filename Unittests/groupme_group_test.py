8359fe91a606af5b65b7e3b7c3e787999346cb99
treefroog <crs2017@gmail.com>
2016-10-16 17:58:49 -0400

    Unit test formatting, and a test to check if all objects in group.members are instances of groupme.Members

--- Unittests/groupme_group_test.py
+++ Unittests/groupme_group_test.py
@@ -10,37 +10,33 @@ import groupme
 class group_test(unittest.TestCase):
 
     def setUp(self):
-
         with open('test_group.txt') as test:
-
             example_json = json.load(test)['response']
 
         self.example_group_object = groupme.Group(**example_json)
 
     def test_group_count(self):
-
         self.assertEqual(self.example_group_object.count, 41)
 
     def test_group_created_at(self):
-
         self.assertEqual(self.example_group_object.created_at, 1474202308)
 
     def test_group_description(self):
-
         self.assertEqual(self.example_group_object.description, '')
 
     def test_group_image_url(self):
-
         self.assertEqual(self.example_group_object.image_url,
         'https://i.groupme.com/214x214.jpeg.51228d02319a4ed9abfa3f2cd7b2d1a6')
 
     def test_group_id(self):
-
         self.assertEqual(self.example_group_object.id, '25081074')
 
     def test_group_max_members(self):
-
         self.assertEqual(self.example_group_object.max_members, 200)
+
+    def test_group_members(self):
+        for member in self.example_group_object.members:
+            self.assertTrue(isinstance(member, groupme.Member))
 
     def test_group_name(self):
 
@@ -65,35 +61,28 @@ class group_test(unittest.TestCase):
                          'Julia Van Goor')
 
     def test_group_preview_author_image(self):
-
         self.assertEqual(self.example_group_object.preview.author_image,
         'https://i.groupme.com/638x638.jpeg.035a2f4a4d8d4ebf970fc69524dc3f5f')
 
     def test_group_preview_created_at(self):
-
         self.assertEqual(self.example_group_object.preview.created_at,
                          1475784598)
 
     def test_group_preview_id(self):
-
         self.assertEqual(self.example_group_object.preview.id,
                          '147578459817776012')
 
     def test_group_preview_text(self):
-
         self.assertEqual(self.example_group_object.preview.text,
                          'My email is 7vangooj@chelsea.k12.mi.us')
 
     def test_group_share_url(self):
-
         self.assertEqual(self.example_group_object.share_url, None)
 
     def test_group_type(self):
-
         self.assertEqual(self.example_group_object.type, 'private')
 
     def test_group_updated_at(self):
-
         self.assertEqual(self.example_group_object.updated_at, 1475784598)
 
 if __name__ == '__main__':
--- unittests/groupme_message_test.py
+++ unittests/groupme_message_test.py
@@ -15,50 +15,39 @@ class message_test(unittest.TestCase):
 		self.example_message_object = groupme.Message(**example_json)
 	
 	def test_message_text(self):
-		
 		self.assertEqual(self.example_message_object.text,
 		"Kira would probably love to write something. Any ideas?")
 	
 	def test_message_created_at(self):
-	
 		self.assertEqual(self.example_message_object.created_at, 1475455189)
 	
 	def test_message_author_name(self):
-	
 		self.assertEqual(self.example_message_object.author.name, "Kira Frahm")
 	
 	def test_author_id(self):
-	
 		self.assertEqual(self.example_message_object.author.id, "30506935")
 	
 	def test_message_author_image_url(self):
-	
 		self.assertEqual(self.example_message_object.author.image_url,
 						"https://i.groupme.com/sms_avatar")
 	
 	def test_message_source_guid(self):
-	
 		self.assertEqual(self.example_message_object.source_guid, 
 						"d069c0c0-6b2f-0134-027f-22000b430990-0")
 	
 	def test_message_id(self):
-	
 		self.assertEqual(self.example_message_object.id, "147545518935507575")
 	
 	def test_message_attachments(self):
-	
 		self.assertEqual(self.example_message_object.attachments, [])
 	
 	def test_message_favorited_by(self):
-	
 		self.assertEqual(self.example_message_object.favorited_by, [])
 	
 	def test_message_group(self):
-	
 		self.assertEqual(self.example_message_object.group, "25081074")
 
 	def test_message_is_system(self):
-
 		self.assertEqual(self.example_message_object.is_system, False)
 
 if __name__ == '__main__':
