4ac8d99f89adc2fad4c88410141ed298008e04fa
Christopher Smyth <crsmyth@mtu.edu>
2021-05-24 00:13:43 -0400

    Relicense to Blue Oak Model

--- groupme/__init__.py
+++ groupme/__init__.py
@@ -3,14 +3,11 @@ Group Me API Wrapper
 ~~~~~~~~~~~~~~~~~~~~
 
 A basic wrapper for the Group Me API.
-
-:copyright: (c) 2016 treefroog
-:license: MIT, see LICENSE for more details.
 """
 
 __title__ = 'groupme'
 __author__ = 'treefroog'
-__license__ = 'MIT'
+__license__ = 'Blue Oak Model'
 __copyright__ = 'Copyright 2016 treefroog'
 __version__ = '0.0.1'
 
